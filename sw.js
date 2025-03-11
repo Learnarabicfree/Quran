a377752d88c64468e046243ef1c1372488c86132
Learnarabicfree <deeninislam98@gmail.com>
2025-03-11 23:45:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,21 @@
-  self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('quranic-wisdom-v1').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/style.css',
-                '/script.js',
-                'https://learnarabicfree.github.io/Quran/img/logo.png',
-                // Add other assets you want to cache
-            ]);
-        })
-    );
+const CACHE_NAME = 'quranic-wisdom-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/img/logo.png',
+  // Add other important assets you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-            return cachedResponse || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
 });
